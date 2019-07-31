4a32498f90ec0b6499454de108b9ef8b510e2455
David Strauch <dmaster18@gmail.com>
2019-07-31 17:45:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  "my_name" = "David",
+  "current_profession" = "CPA", 
+  "future_profession" = "Software Engineer!"
 
   
 end
